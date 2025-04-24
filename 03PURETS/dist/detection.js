039ed55abdc96a679b61758c4787ff5bb1304aa8
Jo-shua09 <joshuaonyeka039@gmail.com>
2025-04-24 20:20:14 +0100

    all good now

--- 03PURETS/dist/detection.js
+++ 03PURETS/dist/detection.js
@@ -1,1 +1,20 @@
 "use strict";
+function detectType(val) {
+    if (typeof val === "string") {
+        return val.toLowerCase();
+    }
+    return val + 3;
+}
+detectType(5);
+console.log(detectType);
+function provideId(id) {
+    if (!id) {
+        console.log("give me an ID");
+    }
+    id === null || id === void 0 ? void 0 : id.toLowerCase();
+}
+function isAdminAccount(account) {
+    if ("isAdmin" in account) {
+        return account.isAdmin;
+    }
+}
---
+++ 03PURETS/src/detection.ts
@@ -0,0 +1,33 @@
+function detectType(val: number | string) {
+  if (typeof val === "string") {
+    return val.toLowerCase();
+  }
+  return val + 3;
+}
+
+detectType(5);
+console.log(detectType);
+
+function provideId(id: string | null) {
+  if (!id) {
+    console.log("give me an ID");
+  }
+  id?.toLowerCase();
+}
+
+interface User {
+  name: string;
+  email: string;
+}
+
+interface Admin {
+  name: string;
+  email: string;
+  isAdmin: boolean;
+}
+
+function isAdminAccount(account: User | Admin) {
+  if ("isAdmin" in account) {
+    return account.isAdmin;
+  }
+}
